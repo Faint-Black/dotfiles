fe598a51352882c13bd5f21b9d849e78acdeefde
Cezar <fntblk@gmail.com>
2024-10-13 19:13:05 -0300

    commit 5

--- sync_files.sh
+++ sync_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # I CANT GET SYMLINKS TO FREAKING WORK
-cp ~/.emacs.d/init.el ./init.el
-cp ~/.gdbinit ./.gdbinit
-cp ~/.config/nvim/init.lua ./init.lua
-cp ~/.terminalWelcomeMessage ./terminal_welcome_message.txt
+cp ~/.emacs.d/init.el        ./emacs/init.el
+cp ~/.gdbinit                ./gdb/.gdbinit
+cp ~/.config/nvim/init.lua   ./neovim/init.lua
+cp ~/.terminalWelcomeMessage ./bash/terminal_welcome_message.txt
