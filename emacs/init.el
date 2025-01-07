80ee25db1606a952c6f92d9e252d73dc30c50dcb
Cezar <fntblk@gmail.com>
2025-01-07 15:37:20 -0300

    commit 12

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,8 @@
-;;------------------------------------------------------------------------
-;; PACKAGE CONFIGURATIONS
-;;------------------------------------------------------------------------
+;;-----------------------------------------------------------------------+
+;; PACKAGE CONFIGURATIONS                                                |
+;;-----------------------------------------------------------------------+
 
-;; set up packaging enviroment
+;; set up package sources and use-package
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("org" . "https://orgmode.org/elpa/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
@@ -12,26 +12,7 @@
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-;; evil-mode boilerplate
-(setq evil-want-keybinding nil)
-(unless (package-installed-p 'evil)
-    (package-refresh-contents)
-    (package-install 'evil))
-(unless (package-installed-p 'evil-collection)
-    (package-refresh-contents)
-    (package-install 'evil-collection))
-(require 'evil)
-(evil-mode 1)
-(require 'evil-collection)
-(evil-collection-init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(evil cmake-mode)))
-
-;; pretty org-mode custom bullet points
+;; pretty org-mode custom headline bullet points
 (use-package org-superstar
     :ensure t
     :hook 
@@ -47,61 +28,76 @@
 
 
 
-;;------------------------------------------------------------------------
-;; EMACS CONFIGURATIONS
-;;------------------------------------------------------------------------
 
-;; set user-specific path configurations (CHANGE THIS FOR YOUR COMPUTER!!)
+;;-----------------------------------------------------------------------+
+;; USER-SPECIFIC FILEPATHS, DONT FORGET TO CHANGE THIS!!                 |
+;;-----------------------------------------------------------------------+
 (setq initial-buffer-choice "~/Desktop/notes/emacs/home.org")
 (setq org-agenda-files '("~/Desktop/notes/emacs/org-files/Calendar.org"))
 
-;; org agenda configurations
-(global-set-key (kbd "C-c a") 'cfw:open-org-calendar)
 
-;; boiler-plate stuff(i forgot what this does)
-(add-hook 'text-mode-hook 'visual-line-mode)
 
-;; base theme
-(load-theme 'wombat t)
 
-;; custom headers and faces
+;;-----------------------------------------------------------------------+
+;; CUSTOM UNIQUE-ONLY EMACS DEFINITIONS                                  |
+;;-----------------------------------------------------------------------+
+
 (custom-set-faces
+    ;; org-mode: calendar framework faces
     '(cfw:face-title ((t (:foreground "#f0dfaf" :weight bold :height 2.0 :inherit variable-pitch))))
     '(cfw:face-header ((t (:foreground "#d0bf8f" :weight bold))))
-    '(cfw:face-sunday ((t :foreground "#a4cbff" :background nil :weight bold)))
-    '(cfw:face-saturday ((t :foreground "#a4cbff" :background nil :weight bold)))
-    '(cfw:face-holiday ((t :foreground "#8c5353" :background nil :weight bold)))
+    '(cfw:face-sunday ((t :foreground "#a4cbff" :background "unspecified" :weight bold)))
+    '(cfw:face-saturday ((t :foreground "#a4cbff" :background "unspecified" :weight bold)))
+    '(cfw:face-holiday ((t :foreground "#8c5353" :background "unspecified" :weight bold)))
     '(cfw:face-grid ((t :foreground "DarkGrey")))
     '(cfw:face-default-content ((t :foreground "#bfebbf")))
     '(cfw:face-periods ((t :foreground "cyan")))
-    '(cfw:face-day-title ((t :background nil)))
+    '(cfw:face-day-title ((t :background "unspecified")))
     '(cfw:face-default-day ((t :weight bold :inherit cfw:face-day-title)))
     '(cfw:face-annotation ((t :foreground "RosyBrown" :inherit cfw:face-day-title)))
     '(cfw:face-disable ((t :foreground "DarkGray" :inherit cfw:face-day-title)))
     '(cfw:face-today-title ((t :background "#dd3333" :weight bold)))
     '(cfw:face-today ((t :background: "#663333" :weight bold)))
     '(cfw:face-select ((t :background "#2f2f2f")))
-    '(cfw:face-toolbar ((t :background nil)))
+    '(cfw:face-toolbar ((t :background "unspecified")))
     '(cfw:face-toolbar-button-off ((t :foreground "gray")))
     '(cfw:face-toolbar-button-on ((t :foreground "white" :weight bold :underline t)))
+    ;; org-mode: faces
     '(org-code ((t (:foreground "#faf678"))))
-    '(org-level-1 ((t (:foreground "#df8735" :weight bold :height 1.4))))
-    '(org-level-2 ((t (:foreground "#9e7ece" :weight bold :height 1.2))))
-    '(org-level-3 ((t (:foreground "#47d055" :weight bold :height 1.1))))
-    '(org-level-4 ((t (:foreground "#87f095" :height 1.05))))
-    '(org-level-5 ((t (:foreground "#f0ce87" :height 1.0))))
-    '(org-level-6 ((t (:foreground "#d1db5e" :height 1.0)))))
+    '(org-level-1 ((t (:foreground "#df8735" :weight bold :height 1.0))))
+    '(org-level-2 ((t (:foreground "#9e7ece" :weight bold :height 1.0))))
+    '(org-level-3 ((t (:foreground "#47d055" :weight bold :height 1.0))))
+    '(org-level-4 ((t (:foreground "#87f095" :weight bold :height 1.0))))
+    '(org-level-5 ((t (:foreground "#f0ce87" :weight bold :height 1.0))))
+    '(org-level-6 ((t (:foreground "#d1db5e" :weight bold :height 1.0)))))
 
-;; disable/enable/customize org-mode faces here 
-(setq org-emphasis-alist
-      '(("*" bold)
-        ("/" italic)
-        ("_" underline)
-        ("=" org-verbatim)
-        ("~" org-code)
-        ("+" (:strike-through t))
-        ("-" (:strike-through t))
-        ("~" (:strike-through t))))
+
+
+
+;;-----------------------------------------------------------------------+
+;; GENERAL EMACS CONFIGURATIONS                                          |
+;;-----------------------------------------------------------------------+
+
+;; base theme
+(load-theme 'wombat t)
+
+;; GUI application settings
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+
+
+
+
+;;-----------------------------------------------------------------------+
+;; ORG-MODE CONFIGURATIONS                                               |
+;;-----------------------------------------------------------------------+
+
+;; org agenda configurations
+(global-set-key (kbd "C-c a") 'cfw:open-org-calendar)
+
+;; boiler-plate stuff (i forgot what this does)
+(add-hook 'text-mode-hook 'visual-line-mode)
 
 ;; text is indented according to it's header depth
 (setq org-startup-indented t)
@@ -121,10 +117,6 @@
         (gnus . gnus)                 ; Open gnus links in a gnus buffer
         (url . browse-url)))          ; Open URL links in the default web browser
 
-;; do not prompt confirmation before running code blocks
+;; do not prompt for confirmation before running org-mode src code blocks
 (setq org-confirm-babel-evaluate nil)
-
-;; bind RETURN to open links in evil mode
-(evil-define-key 'normal 'global
-  (kbd "RET") 'org-open-at-point)
 
